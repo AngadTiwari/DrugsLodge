bc20deee7aa5ec74f8577adfb652ec909f5c0d16
Angad Tiwari <angtwr31@gmail.com>
2015-06-05 23:19:18 +0530

    Init

--- app/src/main/java/com/drugslodge/patient/MapsActivity.java
+++ app/src/main/java/com/drugslodge/patient/MapsActivity.java
@@ -2,7 +2,6 @@ package com.drugslodge.patient;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -44,8 +43,7 @@ public class MapsActivity extends FragmentActivity {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
                 setUpMap();
